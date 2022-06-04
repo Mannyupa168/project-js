f7d318bfd88e4138a6afa896d4d381943cc71a22
Emmanuel Ortega <emmanuel.orteg98@gmail.com>
2022-06-04 06:16:29 -0500

    Adding api to project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+//Catalog
+const cards = document.getElementById('cards');
+const templateCard = document.getElementById('template-card').content;
+
+const fetchData = async () => {
+  try {
+      const res = await fetch('../js/products.json');
+      const data = await res.json();
+      infoProducts(data);
+  }
+  catch (error){
+      console.log(error);
+  };
+};
+
+
+const infoProducts = data => {
+  data.forEach(product => {
+      templateCard.querySelector('h3').textContent = product.productName;
+      templateCard.querySelector('div').textContent = product.price;
+      templateCard.querySelector('img').setAttribute("src", product.imgRoute);
+      const clone = templateCard.cloneNode(true);
+      fragment.appendChild(clone);
+  });
+  cards.appendChild(fragment);
+};
+
 //Log in & Sign up functions
 
 function setFormMessage(formElement, type, message) {
@@ -36,9 +63,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   loginForm.addEventListener("submit", e => {
       e.preventDefault();
-
-      // Perform your AJAX/Fetch login
-
       setFormMessage(loginForm, "error", "Invalid username/password combination");
   });
 
